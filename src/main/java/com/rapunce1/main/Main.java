fbd2605fd2f6564267cc427c5237992229ef325f
rapunce1 <sutyagin_11@mail.ru>
2021-06-08 19:25:58 +0300

    all

--- src/main/java/com/rapunce1/main/Main.java
+++ src/main/java/com/rapunce1/main/Main.java
@@ -18,13 +18,14 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
+        long noRepeatedCounter = 0;
         SAXParserFactory factory = SAXParserFactory.newInstance();
         try {
             SAXParser parser = factory.newSAXParser();
             MyHandler myHandler = new MyHandler();
             parser.parse(new File("address.xml"), myHandler);
 
-            String filename = System.getProperty("user.home") + "\\Desktop\\Result.txt";
+            String filename = System.getProperty("user.home") + "\\Desktop\\XMLParser_Result.txt";
             File file = new File(filename);
             file.createNewFile();
             FileOutputStream fos = new FileOutputStream(file);
@@ -34,26 +35,27 @@ public class Main {
             HashMap<String, City> model = myHandler.getModel();
             List<Struct> repeated = myHandler.getRepeatedLines();
 
-            writer.write("Doubled items:\n");
+            writer.write("Repeated strings:\n");
             for (Struct item :
                     repeated) {
                 writer.write("<item city=\"" + item.data[0] + "\" street=\"" + item.data[1] +
                         "\" house=\"" + item.data[2] + "\" floor=\"" + item.data[3] + "\" /> x" + item.count + "\n\n");
             }
-            writer.write("Total cities: " + model.size() + "\n\n"); // не нужно
 
             Set<String> cityNames = model.keySet();
 
             for (String cityName :
                     cityNames) {
-                City city = model.get(cityName);
+                City city = model.get(cityName);//StringBuilder
                 String result = cityName + '\n';
                 for (int i = 0; i < 5; ++i) {
                     result += i + 1 + " floors: " + city.floors[i] + '\n';
+                    noRepeatedCounter += city.floors[i];
                 }
                 result += '\n';
                 writer.write(result);
             }
+            writer.write("No repeated strings: " + noRepeatedCounter + "\n\n");
             writer.close();
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
